4391a44ba68beb4f5cbea347ae5ab3a5285166ee
Berchez <50505615+Berchez@users.noreply.github.com>
2021-02-27 19:21:50 -0300

    Adicionei o index.py

--- index.py
+++ index.py
@@ -1,47 +1,43 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from time import sleep
 import urllib.request
 import math
 
+imageToBeDonwloaded = input('What Image you want to download ?\n')
+numberOfImages = input('\nHow many images you want to download ?\n')
+numberOfImages = int(numberOfImages)
 
-imageToBeDonwloaded = input('What Image you want to download ?')
-numberOfImages = input('How many images you want to download ?')
+linkPart1 = imageToBeDonwloaded.replace(" ", "-")
+linkPart2 = imageToBeDonwloaded.replace(" ", "%20")
+
+
+numberOfPages = int(numberOfImages / 60)
+RestOfImages = numberOfImages - (numberOfPages * 60)
 
 options = webdriver.ChromeOptions() 
 
 options.add_argument("user-data-dir=C:\\Users\\Berchez\\AppData\\Local\\Google\\Chrome\\AutoBot2") #Path to your chrome profile
 options.add_argument('--profile-directory=Profile 2')
 options.add_argument("start-maximized")
+options.add_argument('headless')
 driver = webdriver.Chrome(executable_path="C:\\Program Files (x86)\\chromedriver.exe", chrome_options=options)
 
 imagemNum = 0
 
-for y in range(30, 80):
-    driver.get(f'https://www.gettyimages.com.br/fotos/{imageToBeDonwloaded}?page={y}&phrase={imageToBeDonwloaded}&sort=mostpopular')
+for y in range(1, (numberOfPages + 1)): 
+    driver.get(f'https://www.gettyimages.com.br/fotos/{linkPart1}?page={y}&phrase={linkPart2}&sort=mostpopular')
     sleep(10)
     content = driver.find_elements_by_class_name('gallery-asset__thumb')
-    for x in range(1, (len(content) - 1)):
+    for x in range(0, (len(content))):
         imagemNum = imagemNum + 1
-        #urllib.request.urlretrieve(content[x].get_attribute("src"), f'./image-{(imagemNum)}.jpg')
-        #sleep(3)
+        urllib.request.urlretrieve(content[x].get_attribute("src"), f'./imagens/image-{(imagemNum)}.jpg')
 
+driver.get(f'https://www.gettyimages.com.br/fotos/{imageToBeDonwloaded}?page={numberOfPages + 1}&phrase={imageToBeDonwloaded}&sort=mostpopular')
+sleep(10)
+content = driver.find_elements_by_class_name('gallery-asset__thumb')
+for z in range(0, RestOfImages):
+    imagemNum = imagemNum + 1
+    urllib.request.urlretrieve(content[z].get_attribute("src"), f'./imagens/image-{(imagemNum)}.jpg')
 
-
-
-print(len(content))
-
-
-# for x in range(0, (len(content) - 1)):
-#     print(f'{content[x]}\n')
-
-
-# urllib.request.urlretrieve({content[0]}, 'image-.jpg')
-
-# for x in range(0, (len(content) - 1)):
-#     urllib.request.urlretrieve(content[x], f'./image-{x}.jpg')
-#     sleep(15)
+driver.quit()
